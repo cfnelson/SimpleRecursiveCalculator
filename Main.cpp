d25ac76737f72958cfa9c2c0e409d06985c2faa0
cfnelson <charles.nelson@griffithuni.edu.au>
2015-04-22 14:36:03 +1000

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,23 @@
 ////////////////////////////////////////////////////////////////////////////////////
 // Filename:		main.cpp
-// Author:			Charles Nelson - s2884951
+// Author:		Charles Nelson - s2884951
 // Date Modified:	12/04/2014 Created
-//					15/04/2014 Updated 
+//			15/04/2014 Updated 
 // Description:		This file contains the main function
 ////////////////////////////////////////////////////////////////////////////////////
 #include "Calculator_H.h"
 
 int main (void)
 {
-	bool quitCalculator = false;																							//- Ensure that the calculator is always running. Bool is never set to true, loop only ends when an error occurs or user selects q || Q.
+	/*** Ensure that the calculator is always running. 
+	     Bool is never set to true, loop only ends when 
+	     an error occurs or user selects q || Q.    **/
+	bool quitCalculator = false;		
 	
-	std::cout << "Hello, I am a simple scientific calculator."									<< std::endl;
-	std::cout << "I accept numbers and the following operators."								<< std::endl;
-	std::cout << "+ , - , * , / , ^ , ( , and )"												<< std::endl;
-	std::cout << "If you wish to exit, enter \"q\" and press ENTER."							<< std::endl;
+	std::cout << "Hello, I am a simple scientific calculator."					<< std::endl;
+	std::cout << "I accept numbers and the following operators."					<< std::endl;
+	std::cout << "+ , - , * , / , ^ , ( , and )"							<< std::endl;
+	std::cout << "If you wish to exit, enter \"q\" and press ENTER."				<< std::endl;
 	std::cout << "Otherwise, please enter in an expression for calculation and press ENTER."	<< std::endl;
 	
 	Calculator calculator; 
@@ -23,14 +26,14 @@ int main (void)
 		double calculatorAnswer = 0;
 	
 		string userInput;
-		getline(cin, userInput);																							//- Retrieving the user's input  and storing it in the string
+		getline(cin, userInput);					//- Retrieving the user's input  and storing it in the string
 
-		calculatorAnswer = calculator.Calculate(userInput.c_str());															//- Returns the calculators answer.Calls the Calculate function with a char* which is the userInput.c_str()
+		calculatorAnswer = calculator.Calculate(userInput.c_str());	//- Returns the calculators answer.Calls the Calculate function with a char* which is the userInput.c_str()
 		
 		std::cout << "Calculating your Answer now." << std::endl;
-		std::cout << "\nAnswer = " << std::fixed	<< std::setprecision(3) << calculatorAnswer << std::endl;
+		std::cout << "\nAnswer = " << std::fixed    << std::setprecision(3) << calculatorAnswer << std::endl;
 		std::cout << "\nIf you want to go again, please enter in an expression and press ENTER."<< std::endl;
-		std::cout << "Otherwise if you wish to exit, enter \"q\" and press ENTER.\n"			<< std::endl;
+		std::cout << "Otherwise if you wish to exit, enter \"q\" and press ENTER.\n"		<< std::endl;
 #ifdef WIN32 
 		system("PAUSE");
 #else
